7cf6fa8a02e5fb03d93d08caea962d6bb169cce5
Kit Langton <kitlangton@gmail.com>
2016-01-15 13:22:27 -0500

    initial commit

---
+++ lib/pairity/version.rb
@@ -0,0 +1,3 @@
+module Pairity
+  VERSION = "0.1.0"
+end
---
+++ spec/pairity_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Pairity do
+  it 'has a version number' do
+    expect(Pairity::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
